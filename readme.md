ffcb29bb31377a4d054e560425a91cec7c0d0afe
msi <msi@DESKTOP-5L6F80Q>
2022-03-12 19:25:42 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+#Hello World
+## A git demonstration
+
+Opincariu Calin
+
+Host: 
+https://github.com/arcq123/HelloWorld
---
+++ src/Tilt/HelloWorld.java
@@ -0,0 +1,10 @@
+package Tilt;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
